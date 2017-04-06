99a1f3d358a74abe75a1f0a93b1bd4f85ae0d53b
dcolam <david.colameo@uzh.ch>
2017-04-06 09:56:34 +0200

    Update mythread.py

--- mythread.py
+++ mythread.py
@@ -9,6 +9,7 @@ import Queue
 import threading
 import time
 from rbp_webdriver import rbp_batch
+from retrieve_sequence_inbatch import startSeqsaving
 
 
 def thread_webdriver(dict_geneGroups):
@@ -38,8 +39,6 @@ def thread_webdriver(dict_geneGroups):
                 queueLock.release()
             time.sleep(1)
     
-    
-    
     threadList = []
     for i in range(0, len(dict_geneGroups)):
         threadList.append('Thread-' + str(i))
@@ -61,6 +60,74 @@ def thread_webdriver(dict_geneGroups):
     queueLock.acquire()
     for key in dict_geneGroups:
         workQueue.put([dict_geneGroups[key], key])
+    queueLock.release()
+    
+    # Wait for queue to empty
+    while not workQueue.empty():
+        pass
+    
+    # Notify threads it's time to exit
+    exitFlag = 1
+    
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+    print "Exiting Main Thread"
+    
+    
+    
+    
+    
+    
+    
+def thread_retrieveseq(dict_geneGroups, path2groups):
+    
+    exitFlag = 0
+
+    class myThread (threading.Thread):
+        def __init__(self, threadID, name, q):
+            threading.Thread.__init__(self)
+            self.threadID = threadID
+            self.name = name
+            self.q = q
+        def run(self):
+            print "Starting " + self.name
+            process_data(self.name, self.q)
+            print "Exiting " + self.name
+    
+    def process_data(threadName, q):
+        while not exitFlag:
+            queueLock.acquire()
+            if not workQueue.empty():
+                data = q.get()
+                queueLock.release()
+                print "%s processing %s" % (threadName, data[1])
+                startSeqsaving(data[0], data[1])
+            else:
+                queueLock.release()
+            time.sleep(1)
+    
+    threadList = []
+    for i in range(0, len(dict_geneGroups)):
+        threadList.append('Thread-' + str(i))
+    #nameList = ["One", "Two", "Three", "Four", "Five"]
+    
+    queueLock = threading.Lock()
+    workQueue = Queue.Queue(10)
+    threads = []
+    threadID = 1
+    
+    # Create new threads
+    for tName in threadList:
+        thread = myThread(threadID, tName, workQueue)
+        thread.start()
+        threads.append(thread)
+        threadID += 1
+    
+    # Fill the queue
+    queueLock.acquire()
+    for key in dict_geneGroups:
+        workQueue.put([dict_geneGroups[key], path2groups[key][0]])
     queueLock.release()
     
     # Wait for queue to empty
